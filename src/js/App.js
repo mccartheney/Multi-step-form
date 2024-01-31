cbe2af427e3fd7373465ec92cec5dd37fc09b3c0
Mccartheney <mccartheney@hotmail.com>
2024-01-31 10:14:18 +0000

    main vars created

---
+++ src/js/App.js
@@ -0,0 +1,24 @@
+//actual step
+let pageNumber = 0;
+
+//----------user info
+let name = undefined;
+let email = undefined;
+let phone = undefined;
+
+//----------plan vars
+//type of plane selectioned by user
+let plan = undefined; //arcade, advanced, pro
+
+//type of payment it can bem per year or per month
+let planType = undefined; //month, year
+
+//price if planType is year the price will multiply 10 times
+let price = {
+    arcade : 9,
+    advanced : 12,
+    pro : 15
+};
+
+//addon var
+let addons = [];
